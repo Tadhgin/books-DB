3ea741dad9f5aa391bff85f8a1f53448eca3b5d3
Tadhgin_White <teddy_bear_morbid@hotmail.com>
2023-04-24 20:09:16 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,106 +1,4 @@
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const { Sequelize } = require('sequelize');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const { registerUser, loginUser } = require('./utils/auth');
-const logoutRouter = require('./public/js/logout.js');
-const errorHandler = require('./errorHandler');
-const booksRouter = require('./controllers/books');
-const Book = require('./models/book');
-const { sequelize, Book, Author } = require('./models');
 
-require('dotenv').config(); // load environment variables from .env file
-{
-    const app = express();
-    // Code that uses app
-  }
-
-{
-    const PORT = process.env.PORT || 3000;
-}
-// Set up session middleware
-app.use(session({
-secret: 'supersecretkey',
-resave: false,
-saveUninitialized: true,
-cookie: {
-secure: false, // set to true if using HTTPS
-maxAge: 1000 * 60 * 60 * 24 // 1 day
-},
-store: new SequelizeStore({
-db: sequelize, // session store using Sequelize
-}),
-}));
-
-// Set up Handlebars as the view engine
-app.engine('handlebars', exphbs({ defaultLayout: 'layout' }));
-app.set('view engine', 'handlebars');
-
-// Define a route for the "new book" page
-app.get('/new-book', (req, res) => {
-res.render('new-book', { pageTitle: 'Add a new book' });
-});
-
-app.post('/books', async (req, res) => {
-    const { title, author, publicationDate } = req.body;
-    const book = await Book.create({ title, author, publicationDate });
-    res.json(book);
-  });
-
-  app.get('/books', async (req, res) => {
-    const books = await Book.findAll();
-    res.json(books);
-  });
-
-// Middleware to parse request bodies
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Serve static files from the public directory
-app.use(express.static('public'));
-
-// Set up routes
-app.use('/logout', logoutRouter); // logout route
-app.use('/books', booksRouter); // books route
-
-// GET route for home page
-app.get('/', async (req, res) => {
-try {
-const books = await Book.findAll(); // get all books from the database using Sequelize
-res.render('index', { books }); // render the index view with the books data
-} catch (error) {
-errorHandler(error, req, res); // handle errors with the errorHandler middleware function
-}
-});
-
-// GET and POST routes for registration
-app.get('/register', (req, res) => {
-res.render('register', { errors: [] }); // render the registration form with empty errors array
-});
-
-app.post('/register', async (req, res) => {
-try {
-await registerUser(req, res); // register the user using the registerUser function from authController
-res.redirect('/login'); // redirect to login page after successful registration
-} catch (error) {
-res.render('register', { errors: [error.message] }); // render the registration form with error message(s)
-}
-});
-
-// GET and POST routes for login
-app.get('/login', (req, res) => {
-res.render('login', { error: null }); // render the login form with null error message
-});
-
-app.post('/login', async (req, res) => {
-try {
-await loginUser(req, res); // log in the user using the loginUser function from authController
-res.redirect('/books'); // redirect to books page after successful login
-} catch (error) {
-res.render('login', { error: error.message }); // render the login form with error message
-}
-});
 
 // Set up database connection using Sequelize
 {
@@ -117,7 +15,7 @@ author: DataTypes.STRING,
 genre: DataTypes.STRING,
 year: DataTypes.INTEGER,
 }, { sequelize });
-
+ 
 class Author extends Model {}
 Author.init({
 name: DataTypes.STRING,
